ba9fd2fea653cf588fcc0ad0df760ed3d1f2f74d
Square Root of Saturn <sqrt@octoblu.com>
2016-06-13 12:30:06 -0700

    Aaron made me.

--- src/containers/create-bluprint.js
+++ src/containers/create-bluprint.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Page } from 'zooid-ui';
 import MeshbluHttp from 'browser-meshblu-http/dist/meshblu-http.js';
 import url from 'url';
 
 import CreateAppForm from '../components/CreateAppForm';
 import { getMeshbluConfig } from '../services/auth-service';
-import { OCTOBLU_URL } from 'config';
+import { OCTOBLU_URL, TOOLS_SCHEMA_REGISTRY_URL } from 'config';
+import superagent from 'superagent';
+import _ from 'lodash';
 
 class CreateBluprint extends React.Component {
   constructor(props) {
@@ -18,13 +20,63 @@ class CreateBluprint extends React.Component {
     };
 
     this.handleCreate = this.handleCreate.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
   }
 
   setErrorState(error) {
     this.setState({
       error,
       loading: false,
+      flowDevice: null,
+      toolsSchema: null,
     });
+  }
+
+  componentWillMount() {
+    const { uuid } = this.props.routeParams;
+    const meshblu = new MeshbluHttp(getMeshbluConfig());
+    meshblu.device(uuid, (error, flowDevice) => {
+      if (error) {
+        return this.setState({ error: error.message });
+      }
+      this.setState({
+        flowDevice,
+        loading: false,
+      });
+    });
+    superagent
+      .get(`${TOOLS_SCHEMA_REGISTRY_URL}`)
+      .end((error, response) => {
+        const { body } = response;
+        if (error) return;
+        this.setState({ toolsSchema: body, loading: false });
+      });
+  }
+
+  componentDidMount() {
+
+  }
+
+  handleUpdate(mappings) {
+    console.log('mappings', mappings)
+    this.configSchema = this.mappingToConfig({mappings});
+  }
+
+  mappingToConfig({ mappings }) {
+    const config = {
+      type: 'object',
+      properties: {},
+    };
+
+    _.each(mappings, function (mapping) {
+      let property = config.properties[mapping.configureProperty];
+      property = property || { type: mapping.type };
+      property['x-node-map'] = property['x-node-map'] || [];
+      property['x-node-map'].push({ id: mapping.nodeId, property: mapping.nodeProperty });
+      config.properties[mapping.configureProperty] = property;
+    });
+
+    return config;
   }
 
   handleCreate(event) {
@@ -35,7 +87,7 @@ class CreateBluprint extends React.Component {
 
     const meshblu = new MeshbluHttp(getMeshbluConfig());
 
-    meshblu.register(this.deviceDefaults({ name: appName.value }), (error, device) => {
+    meshblu.register(this.deviceDefaults({ name: appName.value, configSchema: this.configSchema }), (error, device) => {
       if (error) {
         this.setErrorState(error);
         return;
@@ -55,7 +107,8 @@ class CreateBluprint extends React.Component {
     });
   }
 
-  deviceDefaults({ name }) {
+  deviceDefaults({ name, configSchema }) {
+    console.log('configSchema is', configSchema)
     const USER_UUID = getMeshbluConfig().uuid;
     return {
       name,
@@ -64,6 +117,11 @@ class CreateBluprint extends React.Component {
       type: 'octoblu:bluprint',
       bluprint: {
         version: '1.0.0',
+      },
+      schemas: {
+        configure: {
+          bluprint: configSchema
+        }
       },
       meshblu: {
         version: '2.0.0',
@@ -93,19 +151,27 @@ class CreateBluprint extends React.Component {
   }
 
   render() {
-    const { error, loading } = this.state;
+    const { error, loading, flowDevice, toolsSchema } = this.state;
+    // console.log('render flow', flowDevice)
+    if (flowDevice && toolsSchema) {
+      return (
+        <main>
+          <Page width="small">
+            <CreateAppForm
+              onCreate={this.handleCreate}
+              loading={loading}
+              error={error}
+              flow={flowDevice.flow}
+              toolsSchema={toolsSchema}
+              onUpdate={this.handleUpdate}
+            />
+          </Page>
+        </main>
+      );
+    } else {
+      return null;
+    }
 
-    return (
-      <main>
-        <Page width="small">
-          <CreateAppForm
-            onCreate={this.handleCreate}
-            loading={loading}
-            error={error}
-          />
-        </Page>
-      </main>
-    );
   }
 }
 
