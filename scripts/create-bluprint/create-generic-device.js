d27305ff25b070f120305ac08c73f5b48d6aba9a
Sqrt of Saturn <sqrt@octoblu.com>
2016-06-16 18:21:55 -0700

    Got stuck creating a device for a looong time

--- scripts/create-bluprint/create-generic-device.js
+++ scripts/create-bluprint/create-generic-device.js
@@ -1,54 +1,100 @@
 #!/usr/bin/env node
 var MeshbluHttp = require('meshblu-http')
-var program = require('commander')
+var commander = require('commander')
 
-var server = 'meshblu.octoblu.com:443'
-
-program
+// var meshbluServer = 'meshblu.octoblu.com:443'
+var owner, deviceName, meshbluServer;
+commander
   .version('0.0.1')
-  .option('-o, --owner', 'Set Owner UUID')
-  .option('-n, --name', 'Set Device Name')
-  .option('-s, --server', 'Set meshblu server')
+  .option('-o, --owner <uuid>', 'Set Owner UUID', String)
+  .option('-n, --name <name>', 'Set Device Name', String)
+  .option('-s, --server <server>', 'Set meshblu server', String)
   .parse(process.argv)
 
-if (!program.owner) {
+console.log(commander.owner,  commander.name, commander.server);
+
+if (!commander.owner) {
   console.log('An owner UUID is required, please pass in an owner')
   process.exit(1)
 }
 
-if (program.server) server = program.server
+if (commander.server) meshbluServer = commander.server
 
-console.log('Using Meshblu Server: ' + server)
+console.log('Using Meshblu Server: ' + commander.server)
 
 
 var meshbluHttp = new MeshbluHttp({
-  server: server,
+  server: meshbluServer
 })
 
-meshbluHttp.register({ type: 'device: generic' }, function (error, registeredDevice) {
-  meshbluHttp.update(registeredDevice.uuid, {
-    owner: program.owner,
-    name: program.name || 'Generated Device',
-    schemas: {
-      message: {},
-    },
-    meshblu: {
-      whitelists: {
-        discover: {
-          view: [{ uuid: program.owner }, { uuid: registeredDevice.uuid }],
+var deviceParams =  {
+  owner: commander.owner,
+  name: commander.name || 'Generated Device',
+  "meshblu": {
+      "version": "2.0.0",
+      "whitelists": {
+        "discover": {
+          "view": [
+            {
+              "uuid": "*"
+            },
+            {
+              "uuid": commander.owner
+            }
+          ]
         },
-        configure: {
-          update: [{ uuid: program.owner }, { uuid: registeredDevice.uuid }],
+        "configure": {
+          "update": [
+            {
+              "uuid": commander.owner
+            }
+          ]
+
         },
+      }
+  },
+  schemas: {
+    version: "2.0.0",
+    message: {
+      "example-message-01": {
+        "type": "object",
+        "properties": {
+          "example-opt": {
+            "type": "string",
+            "enum": ["optionA", "optionB", "optionC"]
+          },
+          "another-example-opt":{
+            "type": "string"
+          }
+        },
+        "required": ["example-opt", "another-example-opt"]
       },
-    },
-  }, function (updateError) {
-    if (updateError) {
-      console.error('Permission update failed')
-      process.exit(1)
+      "example-message-02": {
+        "type": "object",
+        "properties": {
+          "some-opt": {
+            "type": "string"
+          },
+          "another-some-opt":{
+            "type": "string"
+          }
+        },
+        "required": ["some-opt", "another-some-opt"]
+      }
     }
+  }
+};
 
-    console.log('Device Created ')
-    console.log(JSON.stringify(registeredDevice, null, 2))
+meshbluHttp.register(deviceParams, function (error, registeredDevice) {
+  console.error(error)
+  var meshbluHttp = new MeshbluHttp({
+    uuid: registeredDevice.uuid,
+    token: registeredDevice.token,
+    server: meshbluServer
   })
-})
+  meshbluHttp.device(registeredDevice.uuid, function(deviceError, updatedDevice){
+    console.log("Device is", deviceError, JSON.stringify(updatedDevice, null, 2));
+    process.exit(0);
+  });
+
+});
