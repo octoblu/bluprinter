9974e1dc31bd51157d9a3426a31f4b328e00c21e
sqrtofsaturn <sqrt@octoblu.com>
2016-07-19 19:06:57 -0700

    Added new container for Finish state when creating bluprint. Needs more styles and details

--- src/containers/FinishCreateBluprint/index.js
+++ src/containers/FinishCreateBluprint/index.js
@@ -1,10 +1,15 @@
 import React, { PropTypes } from 'react'
+import _ from 'lodash'
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
+import { getBluprint } from '../../actions/bluprint'
 import Alert from 'zooid-alert'
 import Button from 'zooid-button'
 import Heading from 'zooid-heading'
 import Page from 'zooid-page'
+import styles from './styles.css'
+import CreateBluprintSteps from '../../components/CreateBluprintSteps'
+
 const propTypes = {
   bluprint: PropTypes.object,
   dispatch: PropTypes.func,
@@ -18,13 +23,35 @@ class FinishCreateBluprint extends React.Component {
 
     this.state = {}
   }
-  componentDidMount(){
+  componentDidMount() {
     const { dispatch, params } = this.props
     dispatch(getBluprint(params.bluprintUuid))
   }
 
   render() {
-    return <div>FinishCreateBluprint</div>
+    const { bluprint } = this.props
+    const { device, error, fetching } = bluprint
+
+    if (fetching) return <Page className={styles.FinishCreateBluprintPage} loading />
+    if (_.isEmpty(device)) return null
+    const { name } = device
+
+    const steps = [
+      { label: 'Create a Bluprint', state: 'COMPLETED' },
+      { label: 'Configure', state: 'COMPLETED' },
+      { label: 'Finish', state: 'ACTIVE' },
+    ]
+    return (
+      <Page className={styles.NewBluprintPage}>
+      <CreateBluprintSteps steps={steps} />
+      <div className={styles.root}>
+        <Heading level={4}>Bluprint: {name}</Heading>
+
+        {error && <Alert type="error">{error.message}</Alert>}
+      </div>
+      </Page>
+
+    )
   }
 }
 
